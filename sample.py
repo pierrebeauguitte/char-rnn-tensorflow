67fed8ad6e7d604ed06e114d7e1513c67d5b7186
Pierre Beauguitte <pierrebeauguitte@gmail.com>
2016-02-18 12:01:28 +0200

    Adapted chunks creation - not optimal, but working

--- sample.py
+++ sample.py
@@ -5,6 +5,7 @@ import argparse
 import time
 import os
 import cPickle
+import random
 
 from utils import TextLoader
 from model import Model
@@ -25,6 +26,12 @@ def sample(args):
         saved_args = cPickle.load(f)
     with open(os.path.join(args.save_dir, 'chars_vocab.pkl')) as f:
         chars, vocab = cPickle.load(f)
+
+    if args.prime == ' ':
+        prime = random.choice(vocab.keys())
+    else:
+        prime = args.prime
+
     model = Model(saved_args, True)
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
@@ -32,7 +39,7 @@ def sample(args):
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print model.sample(sess, chars, vocab, args.n, args.prime)
+            print model.sample(sess, chars, vocab, args.n, prime)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -45,14 +45,71 @@ class TextLoader():
 
     def create_batches(self):
         self.num_batches = self.tensor.size / (self.batch_size * self.seq_length)
-        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
-        xdata = self.tensor
-        ydata = np.copy(self.tensor)
-        ydata[:-1] = xdata[1:]
-        ydata[-1] = xdata[0]
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
-        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
 
+        newline = self.vocab['\n']
+        nextnew = np.where(self.tensor == newline)
+        n_tunes = nextnew[0].size
+
+        print "preparing chunks..."
+        pos = 0
+        n_tune = 0
+        x_chunks = np.array([], dtype=int)
+        y_chunks = np.array([], dtype=int)
+        for nl in nextnew[0]:
+            while (pos + self.seq_length < nl):
+                x_chunks = np.append(x_chunks, self.tensor[pos : pos + self.seq_length])
+                y_chunks = np.append(y_chunks, self.tensor[pos + 1 : pos + self.seq_length + 1])
+                pos += 1
+            n_tune += 1
+            print "done tune ", n_tune, "/", n_tunes
+            pos += self.seq_length
+
+        print "reshaping to seq_length..."
+        x_chunks = np.copy(np.reshape(x_chunks, (-1, self.seq_length)))
+        y_chunks = np.copy(np.reshape(y_chunks, (-1, self.seq_length)))
+
+        print "truncating to match batch_size"
+        x, y = x_chunks.shape
+        self.num_batches = x / self.batch_size
+        print "%i batches" % self.num_batches
+        x_chunks.resize((self.num_batches * self.batch_size, self.seq_length))
+        y_chunks.resize((self.num_batches * self.batch_size, self.seq_length))
+
+        self.x_batches = np.vsplit(x_chunks, self.num_batches)
+        self.y_batches = np.vsplit(y_chunks, self.num_batches)
+        print "batches ready!"
+
+        #### NB ####
+        #
+        # It might be an idea to randomize the X/Y pairs in the batches,
+        # for that we need a tmp array containing them. Some hstack/vsplit involved.
+        # Following is just an unfinished draft of that
+        #
+        ############
+        #
+        # print "stacked: "
+        # xy_stacks = np.hstack((x_chunks, y_chunks))
+        # x, y = xy_stacks.shape
+        # print x
+        # num_batches = x / self.batch_size
+        # print num_batches
+        # xy_stacks.resize(num_batches * self.batch_size, y)
+        # print xy_stacks.shape
+        # xy_stacks.resize((num_batches * self.batch_size * 2, self.seq_length))
+        # print "state of the stuff "
+        # print xy_stacks
+        # # np.random.shuffle(chunks)
+        # # print chunks
+
+        #### ORIGINAL CODE ####
+        # self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        # xdata = self.tensor
+        # ydata = np.copy(self.tensor)
+        # ydata[:-1] = xdata[1:]
+        #### necessary, but quite weird! (y_n = x_0...)
+        # ydata[-1] = xdata[0]
+        # self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
+        # self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
 
     def next_batch(self):
         x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
